75d35efdfb2c83edbd8bc8ee7fd4ec8bc392b835
a-key-bako <31793678+a-key-bako@users.noreply.github.com>
2017-10-27 08:31:36 +0900

    Create dfs34.py

---
+++ aribook/2-1/dfs34.py
@@ -0,0 +1,12 @@
+# 蟻本 p.34 深さ優先探索
+a = [0 for i in range(100000)]
+n, k = 0, 0
+
+def dfs(i, sum):
+    if i == n:
+        return sum == k
+    if dfs(i + 1, sum):
+        return True
+    if dfs(i + 1, sum + a[i]):
+        return True
+    return False
