08cd6a02c1cae4350b6ecd205694563816e6d1ae
a-key-bako <31793678+a-key-bako@users.noreply.github.com>
2017-10-28 18:03:00 +0900

    Update question1.py

--- aribook/2-1/question1.py
+++ aribook/2-1/question1.py
@@ -10,3 +10,41 @@ def dfs(i, sum):
     if dfs(i + 1, sum + a[i]):
         return True
     return False
+
+def solve():
+    if dfs(0, 0):
+        print("Yes")
+    else:
+        print("No")
+
+# input
+n = int(input())
+l = list(map(int, input().split()))
+for i, a_i in enumerate(l):
+    a[i] = a_i
+k = int(input())
+
+solve()
+
+"""
+sample input 1
+
+4
+1 2 4 7
+13
+
+sample output 1
+
+Yes
+
+sample input 2
+
+4
+1 2 4 7
+15
+
+sample output 2
+
+No
+
+"""
