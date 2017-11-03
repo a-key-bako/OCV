2f9603ee54884dc7a2b37750b3cd75c3a84c9bdd
a-key-bako <31793678+a-key-bako@users.noreply.github.com>
2017-11-03 21:27:49 +0900

    Create question6.py

---
+++ aribook/2-2/question6.py
@@ -0,0 +1,37 @@
+# 蟻本 p.45 貪欲法
+def solve():
+    # S[a], a[a+1], S[b]が残っている文字列
+    ans = ""
+    a, b = 0, N-1
+    while a <= b:
+        left = False
+        for i in range(b-a):
+            if S[a+i] < S[b-i]:
+                left = True
+                break
+            elif S[a+i] > S[b-i]:
+                left = False
+                break
+        if left:
+            ans += S[a]
+            a += 1
+        else:
+            ans += S[b]
+            b -= 1
+    print(ans)
+
+# input
+N = int(input())
+S = input()
+
+solve()
+
+"""
+sample input
+6
+ACBBCB
+
+sample output
+ABCBCD
+
+"""
